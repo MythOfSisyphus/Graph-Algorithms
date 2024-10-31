29d0d77800ca70e4da1062d233b9d643b166de25
Lucky <luckychouhan369@gmail.com>
2024-10-31 09:21:42 +0530

    Added explanation of the code and wrote how we tweak the code of union find to get the MST. But StackAbuse has great explanation of this algorithm on their website.

--- Course Material/09_Kruskal_Algorithm.py
+++ Course Material/09_Kruskal_Algorithm.py
@@ -1,6 +1,30 @@
 """
 Kruskal's Algorithm for Finding Minimum Spanning Tree
 It is based on Union-Find (Disjoint-Set) Data Structure.
+
+Stack Abuse has very nice explanation of this algorithm, if you read each line
+there and try to do those steps yourself then you'll get this algorithm.
+The only thing they lack in their explanation is the explanation of the functions
+`find_parent` and `connect_components` so lemme summarize what they're.
+
+If you know about the Union-Find Data Structure then you already know them,
+We try to create tree from give list of nodes. And store each node's parent in
+the dict parent_node at the beginning every node is parent of itself.
+But as we apply the function connect_components parent nodes change.
+
+And usually in Union function of Union-Find we calculate the level of the tree but
+here in Kruskal's Algorithm we calculate the number of nodes in the subtree of each
+node.
+When I first saw the visulization of this algorithm on Wikipedia, I was like how we
+connect the components in a graph, then when I learnt about Union-Find it becomes so clear
+that how simple it is. This is also an example of how small but crucial ideas can solve
+the difficlt looking probem with such easyness.
+
+here in find_parent we not only reture the parent node for given node but keep updating its
+parent node if it is not the parent of itself. Because as the tree gets larger or we have
+so many nodes in components which are dependent on other nodes and those other nodes are also
+dependent on other so it will take time to find parent node for given node, and we'll be doing
+same calculations many times so by doing path compression we can get rid of that.
 """
 def find_parent(parent_node, node):
     if parent_node[node] != node:
@@ -20,14 +44,14 @@ def connect_components(parent_node, component_size, node1, node2):
         parent_node[node1_parent] = node2_parent
         component_size[node2_parent] += component_size[node1_parent]
 
-
 import heapq
 def kruskal_algo(graph):
     parent_node = {node : node for node in graph} # Intialize all nodes as their own parents
     component_size = {node : 0 for node in graph} # As all nodes have no level.
     result = [] # Resulting MST
 
-    myedges = [] # All (edge_weight, node, neighbor) tuples
+    # All edges according to their weights (i.e. in increasing order)
+    myedges = []
     for node in graph:
         neighbors = graph.get(node, [])
         for neighbor, edge_weight in neighbors:
@@ -46,7 +70,6 @@ def kruskal_algo(graph):
             result.append((node1, node2, edge_weight))
             connect_components(parent_node, component_size, node1, node2) # Now connect their respective components
     return result
-#---------------------------------------------------------------
 #---------------------------------------------------------------
 network = {
     'A' : [('B', 4), ('C', 7)],
